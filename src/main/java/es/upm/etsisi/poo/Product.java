b0711eabd8cb883ac474a38b548fc18fe70d6da0
Andrés <andres.lopez.hernandez@alumnos.upm.es>
2025-09-26 23:57:41 +0200

    Inventory, Product and Ticket classes implemented and compiles correctly

--- src/main/java/es/upm/etsisi/poo/Product.java
+++ src/main/java/es/upm/etsisi/poo/Product.java
@@ -1,8 +1,10 @@
 package es.upm.etsisi.poo;
 
-public class Product {
+public class Product
+{
 	
-	public enum Category {
+	public enum Category
+	{
 		MERCH		(0.00f),
 		PAPELERIA	(0.05f),
 		ROPA		(0.07f),
@@ -11,20 +13,59 @@ public class Product {
 
 		private final float discountPercent;
 
-		Category(float discountPercent) {
+		Category(float discountPercent)
+		{
 			this.discountPercent = discountPercent;
 		}
 
-		private float getDiscountPercent() {
+		public  float getDiscountPercent() 
+		{
 			return discountPercent;
 		}
 	}
 	
-	private int id;
+	public final int id;
 	private String name;
 	private Category category;
 	private double price;
 
 	// constructor
-	// getters y setters
+	public Product ( int id, String name, Category category, double price )
+	{
+		this.id = id;
+		this.name = name;
+		this.category = category;
+		this.price = price;
+	}
+
+	public int compareTo ( Product p )
+	{
+		return this.name.compareTo ( p.name );
+	}
+
+	public boolean  equals ( Product p )
+	{
+		return this.id == p.id;
+	}
+	// getters y setters, estos no se usan, son shit code
+
+	public Category category ()
+	{
+		return this.category;
+	}
+
+	public double price ( int amount )
+	{
+		return this.price * amount;
+	}
+
+	@Override
+
+	public String toString ()
+	{
+		return  "{class:Product, id:" + this.id +
+				", name:" + this.name + ", category:" + this.category +
+				", price:" + this.price + "}";
+	}
+
 }
