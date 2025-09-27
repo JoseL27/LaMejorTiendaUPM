423f95df9e38b92be2476b5670b0979f4f73cb8a
Jose <jlv.vallejo27@gmail.com>
2025-09-27 16:16:14 +0200

    Added values for ParseResult representing an invalid command and excessive number of arguments and some error handling for invalid commands

--- src/main/java/es/upm/etsisi/poo/Command.java
+++ src/main/java/es/upm/etsisi/poo/Command.java
@@ -9,24 +9,33 @@ public class Command {
 	public static ParseResult TryParse(String[] tokens) {
 		ParseResult result = null;
 
-		switch (tokens[0]){
-			case "prod":
-				result = ProductCommand.TryParse(tokens);
-				break;
-			case "ticket":
-				//result = TicketCommand.TryParse(tokens);
-				break;
-			case "help":
-				//result = HelpCommand.TryParse(tokens);
-				break;
-			case "echo":
-				//result = EchoCommand.TryParse(tokens);
-				break;
-			case "exit":
-				//result = ExitCommand.TryParse(tokens);
-				break;
-			default:
-				break;
+		if (tokens == null){
+			System.out.println("Error: Null input, not yet implemented");
+		}else if (tokens.length == 0){
+			result = new ParseResult(ParseResult.Code.INSUFICIENT_ARGUMENTS);
+		}else if (tokens.length > 6){
+			result = new ParseResult(ParseResult.Code.TOO_MANY_ARGUMENTS);
+		}else {
+			switch (tokens[0]) {
+				case "prod":
+					result = ProductCommand.TryParse(tokens);
+					break;
+				case "ticket":
+					//result = TicketCommand.TryParse(tokens);
+					break;
+				case "help":
+					//result = HelpCommand.TryParse(tokens);
+					break;
+				case "echo":
+					//result = EchoCommand.TryParse(tokens);
+					break;
+				case "exit":
+					//result = ExitCommand.TryParse(tokens);
+					break;
+				default:
+					result = new ParseResult(ParseResult.Code.INVALID_COMMAND);
+					break;
+			}
 		}
 		return result;
 	}
