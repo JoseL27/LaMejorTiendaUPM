e1259023a563b148c7429c547af37b061fd1b957
Jose <jlv.vallejo27@gmail.com>
2025-09-27 16:01:58 +0200

    First version, waiting for other command classes apart from ProductCommand

--- src/main/java/es/upm/etsisi/poo/Command.java
+++ src/main/java/es/upm/etsisi/poo/Command.java
@@ -1,11 +1,34 @@
 package es.upm.etsisi.poo;
 
+import es.upm.etsisi.poo.commands.ProductCommand;
+
 public class Command {
 	public enum ExecuteResult {
 	}
 	
 	public static ParseResult TryParse(String[] tokens) {
-		return null;
+		ParseResult result = null;
+
+		switch (tokens[0]){
+			case "prod":
+				result = ProductCommand.TryParse(tokens);
+				break;
+			case "ticket":
+				//result = TicketCommand.TryParse(tokens);
+				break;
+			case "help":
+				//result = HelpCommand.TryParse(tokens);
+				break;
+			case "echo":
+				//result = EchoCommand.TryParse(tokens);
+				break;
+			case "exit":
+				//result = ExitCommand.TryParse(tokens);
+				break;
+			default:
+				break;
+		}
+		return result;
 	}
 	
 	public ExecuteResult TryExecute() {
