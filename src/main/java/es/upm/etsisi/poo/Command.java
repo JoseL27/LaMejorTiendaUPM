a50e2da17b19ef369db7f383684e05bffc46cc0b
Jose <jlv.vallejo27@gmail.com>
2025-09-27 16:28:35 +0200

    Documented Command.TryParse

--- src/main/java/es/upm/etsisi/poo/Command.java
+++ src/main/java/es/upm/etsisi/poo/Command.java
@@ -5,7 +5,13 @@ import es.upm.etsisi.poo.commands.ProductCommand;
 public class Command {
 	public enum ExecuteResult {
 	}
-	
+
+	/**
+	 * Tries to parse the command represented by the first element in tokens and calls the corresponding class to do the rest of the parsing,
+	 * unless there is an impossible amount of arguments or the input is null, in which case it returns an error directly
+	 * @param tokens Input representing the command to parse, separated by arguments
+	 * @return ParseResult with the command that was parsed, the corresponding error code if the parsing fails, or null if the input is null
+	 */
 	public static ParseResult TryParse(String[] tokens) {
 		ParseResult result = null;
 
@@ -13,7 +19,7 @@ public class Command {
 			System.out.println("Error: Null input, not yet implemented");
 		}else if (tokens.length == 0){
 			result = new ParseResult(ParseResult.Code.INSUFICIENT_ARGUMENTS);
-		}else if (tokens.length > 6){
+		}else if (tokens.length > 6){ // Currently the highest number of arguments in a command is 6, if this changes this value needs to be updated
 			result = new ParseResult(ParseResult.Code.TOO_MANY_ARGUMENTS);
 		}else {
 			switch (tokens[0]) {
