88da7edadd093f1b7d82d17dc55f5bc8e8b20c11
QTØ <qtnull@nekoc.cc>
2025-09-28 15:53:39 +0200

    add: test in ArrayDataManager (with main)

--- src/main/java/es/upm/etsisi/poo/ArrayDataManager.java
+++ src/main/java/es/upm/etsisi/poo/ArrayDataManager.java
@@ -215,4 +215,245 @@ public class ArrayDataManager implements DataManager {
         }
         return arrayProducts;
     }
+
+    public static void main(String[] args) {
+        // TODO: Refactor this into JUNIT
+        ArrayDataManager adm = new ArrayDataManager();
+
+        System.out.println("Agregar libro");
+        int newProdId = 1;
+        String newProdName = "Libro POO";
+        Product.Category newProdCateg = Product.Category.LIBRO;
+        double newProdPrice = 25;
+        DataResult status = adm.createProduct(newProdId, newProdName, newProdCateg, newProdPrice);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Agregar mismo libro (debe fallar)");
+        status = adm.createProduct(newProdId, newProdName, newProdCateg, newProdPrice);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Agregar libro con ID -2 (debe fallar)");
+        status = adm.createProduct(-2, newProdName, newProdCateg, newProdPrice);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Agregar mismo libro con nombre super largo (debe fallar)");
+        status = adm.createProduct(2, "Libro POOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO", newProdCateg, newProdPrice);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Agregar mismo libro con categoria null (debe fallar)");
+        status = adm.createProduct(2, "Libro POOOOOOOOOOOO", null, newProdPrice);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Agregar mismo libro con precio 0 (debe fallar)");
+        status = adm.createProduct(2, newProdName, newProdCateg, 0);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Agregar mismo libro con precio -2 (debe fallar)");
+        status = adm.createProduct(2, newProdName, newProdCateg, -2.5);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        // Agrego camiseta
+        System.out.println("Agregar camiseta");
+        newProdId = 2;
+        newProdName = "Camiseta talla:M UPM";
+        newProdCateg = Product.Category.ROPA;
+        newProdPrice = 15;
+        status = adm.createProduct(newProdId, newProdName, newProdCateg, newProdPrice);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        // Lista de productos
+        System.out.println("Listado de catalogo");
+        System.out.println("Catalog:");
+        Product[] prodList = adm.listProducts();
+        if (prodList != null) {
+            for (int i = 0; i < prodList.length; i++) {
+                System.out.println(" " + prodList[i]);
+            }
+            System.out.println("prod list: ok");
+        } else {
+            System.out.println("prod list: empty");
+        }
+
+        System.out.println("\n---\n");
+
+        // Actualizar nombre, precio del libro
+        System.out.println("Actualizo nombre del libro");
+        status = adm.updateProductName(1, "Libro POO V2");
+        System.out.println(adm.readProduct(1));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod update: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Actualizo nombre del libro largo (debe fallar)");
+        status = adm.updateProductName(1, "Libro POO V2 OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+        System.out.println(adm.readProduct(1));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod update: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Actualizo precio del libro");
+        status = adm.updateProductPrice(1, 30.0);
+        System.out.println(adm.readProduct(1));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod update: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Actualizo precio del libro con 0 (debe fallar)");
+        status = adm.updateProductPrice(1, 0);
+        System.out.println(adm.readProduct(1));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod update: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Actualizo precio del libro con -4.32 (debe fallar)");
+        status = adm.updateProductPrice(1, -4.32);
+        System.out.println(adm.readProduct(1));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod update: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Inserto un libro repetido y lo borro");
+        newProdId = 3;
+        newProdName = "Libro POO repetido Error";
+        newProdCateg = Product.Category.LIBRO;
+        newProdPrice = 25.0;
+        status = adm.createProduct(newProdId, newProdName, newProdCateg, newProdPrice);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println();
+        // Lista de productos
+        System.out.println("LIST (BEFORE)");
+        System.out.println("Catalog:");
+        prodList = adm.listProducts();
+        if (prodList != null) {
+            for (int i = 0; i < prodList.length; i++) {
+                System.out.println(" " + prodList[i]);
+            }
+            System.out.println("prod list: ok");
+        } else {
+            System.out.println("prod list: empty");
+        }
+
+        System.out.println("\n---\n");
+
+        System.out.println("Lo borro");
+        System.out.println(adm.readProduct(3));
+        status = adm.deleteProduct(3);
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println();
+        // Lista de productos
+        System.out.println("LIST (AFTER)");
+        System.out.println("Catalog:");
+        prodList = adm.listProducts();
+        if (prodList != null) {
+            for (int i = 0; i < prodList.length; i++) {
+                System.out.println(" " + prodList[i]);
+            }
+            System.out.println("prod list: ok");
+        } else {
+            System.out.println("prod list: empty");
+        }
+
+        System.out.println("\n---\n");
+
+        System.out.println("Leer un producto con ID 3 (debe retornar null)");
+        System.out.println(adm.readProduct(3));
+
+        System.out.println("\n---\n");
+
+        System.out.println("Nuevo ArrayDataManager y lo vamos a llevar");
+        adm = new ArrayDataManager();
+        for (int i = 1; i <= 200; i++) {
+            adm.createProduct(i, "Objeto", Product.Category.LIBRO, 32);
+        }
+
+        // Lista de productos
+        System.out.println("LIST (BEFORE)");
+        System.out.println("Catalog:");
+        prodList = adm.listProducts();
+        if (prodList != null) {
+            for (int i = 0; i < prodList.length; i++) {
+                System.out.println(" " + prodList[i]);
+            }
+            System.out.println("prod list: ok");
+        } else {
+            System.out.println("prod list: empty");
+        }
+
+        System.out.println("\n---\n");
+
+        System.out.println("Añado una cosa más (debe fallar)");
+        newProdId = 201;
+        newProdName = "CosaError";
+        newProdCateg = Product.Category.ELECTRONICA;
+        newProdPrice = 65.25;
+        status = adm.createProduct(newProdId, newProdName, newProdCateg, newProdPrice);
+        System.out.println(adm.readProduct(newProdId));
+        if (status == DataResult.SUCCESS) System.out.println("prod add: ok");
+        else System.out.println("prod add: " + status);
+
+        System.out.println("\n---\n");
+
+        System.out.println("Elimino productos con ID pares o divisibles entre 3");
+        for (int i = 1; i <= 200; i++) {
+            if (i % 2 == 0 || i % 3 ==0)
+                adm.deleteProduct(i);
+        }
+
+        // Lista de productos
+        System.out.println("LIST (AFTER)");
+        System.out.println("Catalog:");
+        prodList = adm.listProducts();
+        if (prodList != null) {
+            for (int i = 0; i < prodList.length; i++) {
+                System.out.println(" " + prodList[i]);
+            }
+            System.out.println("prod list: ok");
+        } else {
+            System.out.println("prod list: empty");
+        }
+
+        System.out.println("Test terminado");
+    }
 }
