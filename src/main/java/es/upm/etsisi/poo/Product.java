bcc768f2ebc258269cab1cedbf5092f4cd0e6a30
QTØ <qtnull@nekoc.cc>
2025-09-28 13:07:20 +0200

    add: Product Getter/Setter for its attributes

--- src/main/java/es/upm/etsisi/poo/Product.java
+++ src/main/java/es/upm/etsisi/poo/Product.java
@@ -68,4 +68,31 @@ public class Product
 				", price:" + this.price + "}";
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
