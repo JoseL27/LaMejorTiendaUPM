83c37600d80c39d10e3e37c967c2b4d517ee630e
rochaenrique <rochabenattienrique@gmail.com>
2025-10-02 21:06:54 +0200

    full app tests with separate input and output files, commented out for now

---
+++ src/test/java/es/upm/etsisi/poo/AppTest.java
@@ -0,0 +1,38 @@
+package es.upm.etsisi.poo.test;
+
+import es.upm.etsisi.poo.App;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+	// @Test
+	// void fullAppTest() throws IOException {
+	// 	byte[] inputBytes = Files.readAllBytes(Paths.get("full-app-in.txt"));
+	// 	final InputStream systemIn = System.in;
+	// 	ByteArrayInputStream testIn = new ByteArrayInputStream(inputBytes);
+	// 	System.setIn(testIn);
+
+	// 	final PrintStream systemOut = System.out;
+	// 	ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+	// 	System.setOut(new PrintStream(testOut));
+
+	// 	String expectedOutput = new String(Files.readAllBytes(Paths.get("full-app-expected-out.txt")));
+	// 	assertEquals(expectedOutput, testOut.toString());
+		
+	// 	// Cleanup
+	// 	System.setIn(systemIn);
+	// 	System.setOut(systemOut);		
+	// }
+}
