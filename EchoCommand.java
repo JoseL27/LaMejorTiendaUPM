d4ac55ead050cfdc280615a13422f3990a22d92a
Andrés López Hernández <andres.lopez.hernandez@alumnos.upm.es>
2025-10-03 10:55:47 +0200

    Added classes HelpCommand and EchoCommand

---
+++ EchoCommand.java
@@ -0,0 +1,22 @@
+public class EchoCommand ()
+{
+private final String message
+
+public EchoCommand (String message)
+{
+this.message = message;
+}
+
+public TryExec ()
+{
+ /*if ( message.isEmpty () )
+        {
+            System.err.println ( "Error: echo command requires text to echo" );
+        }
+        else
+        {
+            //System.out.println ( "echo" + text );
+        }*/
+}
+
+}
---
+++ HelpCommand.java
@@ -0,0 +1,15 @@
+package es.upm.etsisi.poo.commands;
+
+import es.upm.etsisi.poo.Command;
+
+public class HelpCommand
+{
+    public static Command.ParseResult TryParse(String[] tokens, Command outCommand) {
+        return null;
+    }
+    
+    public Command.ExecuteResult TryExecute() {
+        return null;
+    }
+
+}
