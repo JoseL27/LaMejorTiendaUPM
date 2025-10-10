f72d338519b35fec982e4b549ea65c1bce036036
Jose <jlv.vallejo27@gmail.com>
2025-10-10 12:24:59 +0200

    Hopefully fixed bug where removing a product from a ticket with more than 1 would throw an exception

--- src/main/java/es/upm/etsisi/poo/Ticket.java
+++ src/main/java/es/upm/etsisi/poo/Ticket.java
@@ -147,8 +147,8 @@ public class Ticket {
 		
 		if (foundIndex != -1) {
 			Product removed = productInfos[foundIndex].getProduct();
-			productInfos[foundIndex] = productInfos[count];
-			productInfos[count]	= null;
+			productInfos[foundIndex] = productInfos[count - 1];
+			productInfos[count - 1]	= null;
 			count--;
 			return removed;
 		}
