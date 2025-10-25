16553884572eb44df971bc8f99a5293c357e93da
Andrés López Hernández <andres.lopez.hernandez@alumnos.upm.es>
2025-10-25 13:21:51 +0200

    parser,  echo and help commands moved to app and a command first parse implemented

--- src/main/java/es/upm/etsisi/poo/App.java
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -32,11 +32,8 @@ public class App {
             System.out.print("tUPM> ");
             command = sc.nextLine();
             if (!command.equals("exit")) {
+                firstParse(command);
                 Parser pars = new Parser(command);
-                Command parsedCommand = Command.tryParse(pars);
-				if (parsedCommand != null) {
-                    parsedCommand.tryExecute(ticket, dataManager);
-				}
             } else {
 				System.out.println("Closing application.");
 				System.out.println("Goodbye!");
@@ -64,6 +61,114 @@ public class App {
             } catch (Exception e) {
                 System.out.println("Error al leer");
             }
+        }
+    }
+
+    private String[] parser(String command) {
+        int n = command.length();
+        commands = new String[n];
+        for (int k = 0; k < n; k++) {
+            commands[k] = "";
+        }
+        auxParser(command, commands);
+        cutter( commands );
+        return commands;
+    }
+
+    private void auxParser(String command, String[] commands) {
+        final String text = command.trim().replaceAll(" +", " ");     //Elimina espacios
+        int i = 0;
+        boolean comillas = false;
+        for (int j = 0; j < text.length(); j++) {
+            char ch = text.charAt(j);
+            if (ch == ' ' && !comillas) {
+                i++;
+                commands[i] = "";
+            } else if (ch == '"') {
+                comillas = !comillas;
+            } else {
+                commands[i] += ch;
+            }
+        }
+
+    }
+
+    private void cutter(String[] commands) {
+        int i = 0;
+        while ((i < commands.length) && (commands[i] != "")) {
+            i++;
+        }
+        String[] aux = new String[i];
+        for (int j = 0; j < i; j++) {
+            aux[j] = commands[j];
+        }
+        commands = aux;
+    }
+
+    private void echo(String message) { 
+        if ( text.isEmpty () )
+        {
+            System.err.println ( "Error: echo command requires text to echo" );
+        }
+        else
+        {
+            System.out.println ( "echo" + text );
+        }
+		//System.out.printf("echo \"%s\"\n", message); not needed, firstParse calles echo with message as parameter
+    }
+
+    private void help() {
+        System.out.println("Commands:");
+        System.out.println(" prod add <id> \"<name>\" <category> <price>");
+        System.out.println(" prod list" );
+		
+        System.out.printf(" prod update <id> %s <value>\n", Utils.arrayToString(Product.Field.values(), "|"));
+		
+        System.out.println(" prod remove <id>");
+        System.out.println(" ticket new");
+        System.out.println(" ticket add <prodId> <quantity>");
+        System.out.println(" ticket remove <prodId>");
+        System.out.println(" ticket print" );
+        System.out.println(" echo \"<texto>\"");
+        System.out.println(" help");
+        System.out.println(" exit");
+		System.out.println();
+
+		Product.Category[] categoryValues = Product.Category.values();
+        System.out.printf("Categories: %s\n", Utils.arrayToString(categoryValues, ", "));
+        System.out.print("Discounts if there are ≥2 units in the category: ");
+
+		for (int i = 0; i < categoryValues.length; i++) { 
+			System.out.printf("%s %.0f%%", categoryValues[i].name(), categoryValues[i].getDiscountPercent()*100);
+			if (i  < categoryValues.length - 1) {
+				System.out.print(", ");
+			}
+        }
+		
+        System.out.println(".");
+    }
+
+    private static void firstParse ( String entrada )
+    {
+        String[] tokenized = parser( entrada );
+        final String command = tokenized[0];
+        switch ( command )
+        {
+            case "prod":
+                //parseProduct ( text ); reemplazar con llamada a ProductCommand
+                break;
+            case "ticket":
+                //parseTicket ( text ); reemplazar con llamada a TicketCommand
+                break;
+            case "echo":
+                echo ( tokenized[1] );
+                break;
+            case "help":
+                help ();
+                break;
+            case null:
+                System.err.println("Command not recognized");
+                break;
         }
     }
 
