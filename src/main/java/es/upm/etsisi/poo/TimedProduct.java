eb4319c29b4824c1949c56266e89075e584c10f6
rochaenrique <rochabenattienrique@gmail.com>
2025-11-07 14:10:32 +0100

    applied changes to timed product

--- src/main/java/es/upm/etsisi/poo/TimedProduct.java
+++ src/main/java/es/upm/etsisi/poo/TimedProduct.java
@@ -4,7 +4,7 @@ public class TimedProduct extends Product {
     // timeForPreparing are de ms minimun to prepare  the activity.
     public enum TimedType {
         MEETING(43200000), //12h
-        LAUNCH(259200000); //72h
+        FOOD(259200000); //72h
 
         private int  timeForPreparing;
 
@@ -17,12 +17,14 @@ public class TimedProduct extends Product {
     private static final int TIMED_PRODUCT_MAX_PEOPLE = 100;
 	
     private TimedType type;
+    private int amount;
     private int maxParticipants;
 
     // It is assumed that all the parameters are valid, this should be handled before creating the object
     public TimedProduct(int id, String name, double individualPrice, int maxParticipants, TimedType type) {
         super(id, name, individualPrice);
         this.type = type;
+		this.amount = maxParticipants;
         this.maxParticipants = maxParticipants;
     }
 
@@ -30,26 +32,28 @@ public class TimedProduct extends Product {
         return this.type;
     }
 
+	public int getAmount() {
+		return this.amount;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
     public int getMaxParticipants() {
         return this.maxParticipants;
     }
-
-	@Override
-	public double getMultipliedPrice(int amount) {
-		return amount * maxParticipants * super.getPrice();
-	}
-
-	// NOTE(enrique): A TimedProduct is multiple only of other TimedProduct that don't have the same id as it.
-	// Meaning you cannot hold two TimedProducts of the same id in a ticket.
-	@Override
-	public boolean canDuplicate() {
-		return false;
-	}
 	
 	@Override
 	public boolean duplicateOf(Product product) {
 		return (product != null) 
 			&& product.getClass() == this.getClass()
 			&& product.getId() != this.getId();
+	}
+
+	@Override
+	public String toString() {
+        return String.format("{id:%d, name:'%s', type:%s, price:%.1f}",
+							 this.id, super.getName(), this.type, super.getPrice());
 	}
 }
