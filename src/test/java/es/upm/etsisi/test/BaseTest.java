3ebb19dddb5ff430a3ca58613e2635bc6891a648
rochaenrique <rochabenattienrique@gmail.com>
2026-01-13 22:17:47 +0100

    clearing serialize after every test

--- src/test/java/es/upm/etsisi/test/BaseTest.java
+++ src/test/java/es/upm/etsisi/test/BaseTest.java
@@ -2,28 +2,31 @@ package es.upm.etsisi.test;
 /* date = December 24th 2025 7:40 pm */
 
 import es.upm.etsisi.poo.UserManager;
+import es.upm.etsisi.poo.Serialize;
 import es.upm.etsisi.poo.Inventory;
 
 import java.util.Locale;
+import java.util.Map;
 import java.time.LocalDateTime;
 import java.lang.reflect.Field;
+import java.io.Serializable;
 
 import org.junit.jupiter.api.*;
 
 public class BaseTest { 
 	// DICTATOR LOCALE 
     @BeforeAll
-        private static void setEnUSLocale() {
+		public static void setEnUSLocale() {
         Locale.setDefault(new Locale("en", "US"));
     }
     
     @AfterAll
-        private static void unsetEnUSLocale() {
+		public static void unsetEnUSLocale() {
         Locale.setDefault(Locale.getDefault());
     }
     
     @BeforeEach
-        private void clearSingletons() {
+		public void clearSingletons() {
         try {
             Field f; 
             
@@ -34,6 +37,11 @@ public class BaseTest {
             f = UserManager.class.getDeclaredField("instance");
             f.setAccessible(true);
             f.set(null, null);
+			
+			f = Serialize.class.getDeclaredField("dataStore");
+			f.setAccessible(true);
+			Map<String, Serializable> dataStore = (Map<String, Serializable>)f.get(null);
+			dataStore.clear();
             
         } catch (Exception e) {
         }
