23d239570b96afa1b088c7011ce1ba791c1e65f7
rochaenrique <rochabenattienrique@gmail.com>
2026-01-18 11:12:24 +0100

    added email checking to use constructor

--- src/main/java/es/upm/etsisi/poo/User.java
+++ src/main/java/es/upm/etsisi/poo/User.java
@@ -1,5 +1,6 @@
 package es.upm.etsisi.poo;
 
+import es.upm.etsisi.poo.exceptions.InvalidDataException;
 import java.io.Serializable;
 
 public abstract class User implements Serializable {
@@ -7,7 +8,12 @@ public abstract class User implements Serializable {
     private String name;
     private String email;
     
-    public User(String id, String name, String email){
+    public User(String id, String name, String email) throws InvalidDataException {
+		
+		if (!isValidEmail(email)) {
+            throw new InvalidDataException("Invalid user email: '" + email + "'");
+		}
+		
         this.id = id;
         this.name = name;
         this.email = email;
@@ -32,4 +38,16 @@ public abstract class User implements Serializable {
         User other = (User)obj;
         return this.id.equals(other.id); // ID should be a globally unique identifier for any User (Client/Cashier/etc)
     }
+	
+	public static boolean isValidEmail(String email) {
+		boolean result = false;
+		
+		String[] splitEmail = email.split("@");
+		
+		if (splitEmail.length == 2) {
+			result = splitEmail[0].length() > 0 && 
+				splitEmail[1].length() > 0;
+		}
+		return result;
+	}
 }
